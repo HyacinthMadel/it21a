1ed84a52df3a88b449ca3aaaad3791da4aa32641
HyacinthMadel <hyacinth.udtohan08@gmail.com>
2025-09-15 08:23:53 +0800

    Added warrioe subclass and updated object instance

---
+++ hero.js
@@ -0,0 +1,21 @@
+class Hero{
+    #health;
+
+    constructor (name,health,attack){
+        this.name = name;
+        this.#health = health;
+        this.attack = attack;
+    }
+
+    getHealth(){
+        return this.#health = this.#health;
+    }
+}
+
+class Warrior extends Hero{
+    useAbility(){
+        console.log('${this.name} uses power Strike');
+    }
+}
+const warrior = new Warrior("Thorin",100,10);
+warrior.useAbility();
