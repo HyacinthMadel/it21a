4f8887eec81ac7860299430b89449bab2fad455c
HyacinthMadel <hyacinth.udtohan08@gmail.com>
2025-09-15 08:35:33 +0800

    Added Gandalf

--- hero.js
+++ hero.js
@@ -8,14 +8,27 @@ class Hero{
     }
 
     getHealth(){
-        return this.#health = this.#health;
+        return this.#health;
     }
 }
 
 class Warrior extends Hero{
     useAbility(){
-        console.log('${this.name} uses power Strike');
+        console.log(`${this.name} uses power Strike`);
+    }
+}
+
+class Mage extends Hero{
+    constructor(name,health,attack,mana){
+        super(name,health,attack);
+        this.mana = mana;
+    }
+    useAbility(){
+        console.log(`${this.name} casts Fireball!`);
     }
 }
 const warrior = new Warrior("Thorin",100,10);
 warrior.useAbility();
+
+const mage = new Mage("Gandalf",80,8,50);
+mage.useAbility();
