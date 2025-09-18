f7774c70f25403c178dcb6a1947ec5a6064143dc
zeelikeswhale <zeenuttahoe@gmail.com>
2025-09-18 08:06:58 +0800

    added abstruction work

--- hero.js
+++ hero.js
@@ -10,6 +10,13 @@ class Hero{
     getHealth(){
         return this.#health;
     }
+
+    getStats(){
+        console.log("\n");
+        console.log("Name: "+ this.name);
+        console.log("Health: "+ this.#health);
+        console.log("Attack: "+ this.attack);
+    }
 }
 
 class Warrior extends Hero{
@@ -27,8 +34,26 @@ class Mage extends Hero{
         console.log(`${this.name} casts Fireball!`);
     }
 }
+
+function performAbility(hero){
+    hero.useAbility();
+}
+
+performAbility(Thorin);
+performAbility(Gandalf);
+
 const warrior = new Warrior("Thorin",100,10);
-warrior.useAbility();
+//warrior.useAbility();
+Thorin.getStats();
 
 const mage = new Mage("Gandalf",80,8,50);
-mage.useAbility();
+//mage.useAbility();
+Gandalf.getStats();
+
+function performAbility(hero){
+    console.log("\n");
+    hero.useAbility();
+}
+
+performAbility(Thorin);
+performAbility(Gandalf);
